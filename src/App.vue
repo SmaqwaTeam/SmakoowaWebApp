23fe9db59a0c88fd047dbb2d04faef3bfaa2794d
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2023-04-14 01:25:27 +0200

    test CORS

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import {useRecipesStore} from "./stores/RecipesStore";
+
 import { mapState } from 'pinia';
 export default {
   data() {
@@ -11,7 +12,11 @@ export default {
   computed: {
     ...mapState(useRecipesStore,{
       test: "test"
+    }),
+    ...mapState(useUserStore,{
+      user: "user"
     })
+
   },
   methods: {
     toggleMenu(){
@@ -36,7 +41,7 @@ export default {
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between  mx-auto gap-3 md:gap-4  p-4">
       <a href="#" class="flex items-center">
       <img src="./assets/logoSmakoowa.png" class="h-8 mr-3" alt="Flowbite Logo">
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Smakoowa</span>
+      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Smakoowa </span>
       </a>
         <div class="flex md:order-2" >
           <button @click="goToLogin" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">Log in</button>
--- src/stores/RecipesStore.js
+++ src/stores/RecipesStore.js
@@ -1,9 +1,19 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 
 export const useRecipesStore = defineStore("RecipeStore", {
     state: () => {
         return {
             test: 2137
+        }
+    },
+    actions: {
+        async getCategories() {
+           try{
+            const res = axios.get(import.meta.env.VITE_API_BACKEND+"/api/Account/Register")
+           }catch(error){
+
+           }
         }
     }
 })
