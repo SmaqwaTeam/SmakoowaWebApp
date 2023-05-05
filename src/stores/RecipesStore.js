bc3644103e554aa4ef884d3a0c47fcb859bf662f
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2023-05-06 01:09:53 +0200

    code cleanup

--- src/stores/RecipesStore.js
+++ src/stores/RecipesStore.js
@@ -98,44 +98,5 @@ export const useRecipesStore = defineStore("RecipeStore", {
                 console.log(error)
             }
         },
-         async submitRecipe(payload){
-            console.log(payload)
-            try{
-                const res = await fetch(
-                    import.meta.env.VITE_API_BACKEND+"/api/Recipes/Create",
-                      {
-                        method: "POST",
-                        headers: {
-                          'Content-Type': 'application/json',
-                          'accept': 'text/plain',
-                          'Authorization': 'Bearer '+localStorage.getItem('userToken')
-                        },
-                        body:JSON.stringify({
-                          "name":payload.name,
-                          "description":payload.description,
-                          "servingsTier":parseInt( payload.servingsTier),
-                          "timeToMakeTier":parseInt(payload.timeToMakeTier),
-                          "categoryId": payload.categoryId,
-                          "tagIds":payload.tagIds,
-                          "ingredients":payload.ingredients,
-                          "instructions":payload.instructions,
-                        })
-                      }
-                  );
-                  const data = await res.json();
-                  console.log(data)
-                if(data.successStatus)
-                {
-                    console.log(data.message)
-                    this.$router.push('/profile')
-                }
-                else
-                {
-                    alert(data.message)
-                }
-            }catch(error){
-                console.log(error)
-            }
-        },
     }
 })
