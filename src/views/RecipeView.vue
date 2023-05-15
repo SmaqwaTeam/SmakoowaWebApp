b3b10c5b7b43b0bc72b4bffb316a6e70030f6e72
mikolajsniechowski <mikolaj.sniechowski@stud.pwsz.legnica.edu.pl>
2023-05-15 14:37:10 +0200

    image display fix 5

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -18,7 +18,7 @@
         </div>
         <hr />
         <div
-          class="h-auto max-w-full rounded-lg border-orange-300 border-solid border-2 md:max-w-1/2 mx-auto my-2"
+          class="h-auto max-w-full rounded-lg border-orange-300 border-solid border-2 md:min-w-1/2  mx-auto my-2"
         >
           <img
             v-if="imagesrc"
